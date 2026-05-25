91111d69a06abf0d20a7d76497c30b1d85ae863a
siganushka <siganushka@gmail.com>
2026-05-25 12:01:51 +0800

    refactor: update trans message

--- translations/messages.en.php
+++ translations/messages.en.php
@@ -3,4 +3,9 @@
 declare(strict_types=1);
 
 return [
+    'transaction.number' => 'Number',
+    'transaction.amount' => 'Amount',
+    'transaction.type' => 'Type',
+    'transaction.pay_method' => 'Pay Method',
+    'transaction.successful' => 'Successful',
 ];
